0598dfecd15e1f76e48774b3b18d5b421a5fa70a
manarm <mitchell@manarm.co>
2021-03-24 20:50:56 -0500

    add users fetch

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(express.json());
 
 const login = require('./login');
-const tasks = require('./items')
+const tasks = require('./items');
+const users = require('./users');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +16,7 @@ app.get('/', (req, res) => {
 app.post('/login', login.handleLogin);
 app.get('/items', tasks.getItems);
 app.post('/items', tasks.updateItem)
+app.get('/users', users.getUsers);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
